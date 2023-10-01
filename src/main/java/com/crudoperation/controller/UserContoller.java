2ec6e2111cccc90d70f6e9d641cbdd645dd6ea74
Ashish Yadav <aybhu10@gmail.com>
2023-10-01 13:59:05 +0530

    Cross origin

--- src/main/java/com/crudoperation/controller/UserContoller.java
+++ src/main/java/com/crudoperation/controller/UserContoller.java
@@ -5,6 +5,7 @@ import com.crudoperation.model.User;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.crudoperation.service.UserService;
 
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserContoller {
